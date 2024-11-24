022043afbe6ce78459cde18f441a6f457c39a9fe
jeremy-dhy98 <mulwajeremy24@gmail.com>
2024-11-24 07:36:02 +0300

    Modified some files & added new

--- GEMINI_AI/test_gemini.py
+++ GEMINI_AI/test_gemini.py
@@ -10,8 +10,10 @@ model = genai.GenerativeModel("gemini-1.5-flash")
 
 # Upload file
 file_path = r"VID-20241121-WA0003.mp4"
-print("Uploading file...")
+
 def upload():
+    """Än attempt to upload a file"""
+    print("Uploading file...")
     try:
         file = genai.upload_file(file_path)
         print(f"Uploaded file: {file.name}")
@@ -20,12 +22,28 @@ def upload():
         pass
     else:
          return file
+
+# Save generated contents
+def response_in_text(path):
+    """Än attempt to save a file with response from model"""
+    print(f"Saving file to: {path}")
+    try:
+      with open(path, "w", encoding="utf-8") as fl_obj:
+          fl_obj.write(response.text)
+          print("File saved sucessfully...")
+    except:
+        pass
+   
     
 # Define prompt--> text. image, video, audio
-file = upload()
-prompt = "Generate a python script which executes the sound in this video\
-          when expressed in text format."
-response = model.generate_content([file, "\n\n", prompt])
+# file = upload()
+# prompt = "Generate a python script which executes the sound in this video\
+#           when expressed in text format."
+# [file, "\n\n", prompt]
+prompt = "Why choose python?"
+response = model.generate_content(prompt)
 
+# Call method to save the response
+response_in_text("why_python.txt")
 print(response.text) # print output as text
 
